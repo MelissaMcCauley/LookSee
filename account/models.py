ef6f6aa5a21ae02c5157843a1341f6a658b16867
mmccauley <none@noemail.com>
2021-02-18 20:35:20 -0500

    Added intermediary Contact model to handle following relationships between users and also monkey patched the 'following' field to the Django User model

--- account/models.py
+++ account/models.py
@@ -1,5 +1,31 @@
-from django.db import models
 from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.db import models
+
+class Contact(models.Model):
+    user_from = models.ForeignKey('auth.User',
+                                  related_name='rel_from_set',
+                                  on_delete=models.CASCADE)
+    user_to = models.ForeignKey('auth.User',
+                                related_name='rel_to_set',
+                                on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True,
+                                   db_index=True)
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return f'{self.user_from} follows {self.user_to}'
+        
+
+# Add the following field to User dynamically
+user_model = get_user_model()
+user_model.add_to_class('following',
+                        models.ManyToManyField('self',
+                        through=Contact,
+                        related_name='followers',
+                        symmetrical=False))
 
 
 class Profile(models.Model):
@@ -9,3 +35,5 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'Profile for user {self.user.username}'
+
+
